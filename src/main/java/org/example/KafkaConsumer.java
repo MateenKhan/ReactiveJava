2840c2b3011ea9c4f47c69db75afcf0bbc946cc2
Mateen <makjavaprogrammer@gmail.com>
2022-11-17 00:51:26 +0530

    first commit

---
+++ src/main/java/org/example/KafkaConsumer.java
@@ -0,0 +1,20 @@
+package org.example;
+
+import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.kafka.support.KafkaHeaders;
+import org.springframework.messaging.handler.annotation.Header;
+import org.springframework.stereotype.Component;
+
+import java.util.List;
+
+@Component
+public class KafkaConsumer {
+
+    @KafkaListener(topics = "test-topic")
+    public void processMessage(String message,
+                               @Header(KafkaHeaders.RECEIVED_PARTITION_ID) List partitions,
+                               @Header(KafkaHeaders.RECEIVED_TOPIC) List topics,
+                               @Header(KafkaHeaders.OFFSET) List offsets) {
+        System.out.printf("%s-%d[%d] \"%s\"\n", topics.get(0), partitions.get(0), offsets.get(0), message);
+    }
+}
---
+++ src/main/java/org/example/KakfaProducer.java
@@ -0,0 +1,37 @@
+package org.example;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@Service
+@RestController
+public class KakfaProducer {
+    private static final Logger LOGGER = LoggerFactory.getLogger(KakfaProducer.class);
+
+
+    @Autowired
+    private KafkaTemplate<String, String> kafkaTemplate;
+
+    public void sendMessage(PostRequest postRequest){
+        LOGGER.info(String.format("Message sent -> %s", postRequest.getMessage()));
+        kafkaTemplate.send(postRequest.getTopic(), postRequest.getMessage());
+    }
+
+    @PostMapping("/produce")
+    public String produce(@RequestBody PostRequest postRequest){
+        sendMessage(postRequest);
+        return "success";
+    }
+
+    @GetMapping
+    public String test(){
+        return "test";
+    }
+}

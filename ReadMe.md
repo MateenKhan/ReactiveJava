be0474fd927ae9a4b1b31f62bc8846957315693f
Mateen <makjavaprogrammer@gmail.com>
2022-11-17 04:13:31 +0530

    enabled kafka consumer and updated ReadMe.md

--- ReadMe.md
+++ ReadMe.md
@@ -1,6 +1,6 @@
 ## Pre-requisites
-1. Java 11
-2. docker
+1. [Java 11](https://www.azul.com/downloads/?version=java-11-lts&package=jdk)
+2. [docker](https://docs.docker.com/get-docker/)
 
 ## Installation
 Docker command to run the kafka and akhq
@@ -9,6 +9,8 @@ docker-compose  -f compose.yml up
 ```
 
 click: [akhq](http://localhost:8080/) to check kafka 
+
+click: [swagger](http://localhost:9090/swagger-ui.html) to test from swagger ui
 
 click: [h2](http://localhost:9090/h2-console) to check db
     
--- src/main/java/org/example/kafka/KafkaConsumer.java
+++ src/main/java/org/example/kafka/KafkaConsumer.java
@@ -1,20 +1,20 @@
-//package org.example.kafka;
-//
-//import org.springframework.kafka.annotation.KafkaListener;
-//import org.springframework.kafka.support.KafkaHeaders;
-//import org.springframework.messaging.handler.annotation.Header;
-//import org.springframework.stereotype.Component;
-//
-//import java.util.List;
-//
-//@Component
-//public class KafkaConsumer {
-//
-//    @KafkaListener(topics = "test-topic")
-//    public void processMessage(String message,
-//                               @Header(KafkaHeaders.RECEIVED_PARTITION_ID) List partitions,
-//                               @Header(KafkaHeaders.RECEIVED_TOPIC) List topics,
-//                               @Header(KafkaHeaders.OFFSET) List offsets) {
-//        System.out.printf("%s-%d[%d] \"%s\"\n", topics.get(0), partitions.get(0), offsets.get(0), message);
-//    }
-//}
+package org.example.kafka;
+
+import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.kafka.support.KafkaHeaders;
+import org.springframework.messaging.handler.annotation.Header;
+import org.springframework.stereotype.Component;
+
+import java.util.List;
+
+@Component
+public class KafkaConsumer {
+
+    @KafkaListener(topics = "test-topic")
+    public void processMessage(String message,
+                               @Header(KafkaHeaders.RECEIVED_PARTITION_ID) List partitions,
+                               @Header(KafkaHeaders.RECEIVED_TOPIC) List topics,
+                               @Header(KafkaHeaders.OFFSET) List offsets) {
+        System.out.printf("%s-%d[%d] \"%s\"\n", topics.get(0), partitions.get(0), offsets.get(0), message);
+    }
+}
